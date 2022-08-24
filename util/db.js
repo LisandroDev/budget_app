1eaad5ec6002f43b5c503f199c0cc9addac94bec
lichaa824 <lisandrorubi@gmail.com>
2022-08-24 14:38:18 -0300

    Add migrations handler

--- util/db.js
+++ util/db.js
@@ -1,5 +1,6 @@
 const { DATABASE_URL } = require("./config");
 const Sequelize = require("sequelize");
+const { Umzug, SequelizeStorage } = require("umzug");
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
@@ -10,9 +11,26 @@ const sequelize = new Sequelize(DATABASE_URL, {
   },
 });
 
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: "migrations/*.js",
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  });
+
+  const migrations = await migrator.up();
+  console.log("Migrations up to date ", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     console.log("Connected to the database");
   } catch (error) {
     console.log("Connection attempt failed");
@@ -21,4 +39,4 @@ const connectToDatabase = async () => {
   return null;
 };
 
-module.exports = { connectToDatabase };
+module.exports = { connectToDatabase, sequelize };
