e04c6a82dfc988aa34649c60d84cabc26a7fbf81
lichaa824 <lisandrorubi@gmail.com>
2022-09-01 21:54:05 -0300

    Delete boilerplate

--- frontend/budget-app/src/index.js
+++ frontend/budget-app/src/index.js
@@ -4,12 +4,14 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import transactionReducer from "./reducers/transactionReducer";
 import App from "./App";
+import "./styles/color.css"
 
 const store = configureStore({
   reducer: {
     transactions: transactionReducer,
   },
 });
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
