e17f3613b2603f2d0028213a9687a2353f0a4ee9
lichaa824 <lisandrorubi@gmail.com>
2022-10-10 17:28:48 -0300

    add form state

--- frontend/budget-app/src/components/AbmSection/AbmForm.js
+++ frontend/budget-app/src/components/AbmSection/AbmForm.js
@@ -1,42 +1,71 @@
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
 
 const AbmForm = () => {
+  const [formState, setFormState] = useState({
+    concept: "",
+    amount: 0,
+    type: "income",
+    date: new Date()
+  });
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value });
+  };
+
   return (
     <Form>
       <Form.Group className="mb-3">
         <Form.Label>Concept</Form.Label>
-        <Form.Control placeholder="Concept"></Form.Control>
+        <Form.Control
+          placeholder="Concept"
+          name="concept"
+          id="concept"
+          onChange={handleChange}
+        ></Form.Control>
       </Form.Group>
       <Form.Group className="mb-3">
         <Form.Label>Type of transaction</Form.Label>
         <div>
           <Form.Check
             inline
-            checked="checked"
+            defaultChecked
             type="radio"
-            name="group1"
+            value="income"
+            name="type"
+            onChange={handleChange}
             label="Income"
-            id="income-radio"
+            id="income"
           />
           <Form.Check
             inline
             type="radio"
-            name="group1"
+            name="type"
+            value="expense"
             label="Expense"
-            id="expense-radio"
+            onChange={handleChange}
+            id="expense"
           />
         </div>
       </Form.Group>
       <Form.Group>
-          <Form.Label>Amount</Form.Label>
-          <Form.Control placeholder="Amount" type="number"></Form.Control>
+        <Form.Label>Amount</Form.Label>
+        <Form.Control
+          placeholder="Amount"
+          onChange={handleChange}
+          id="amount"
+          name="amount"
+          type="number"
+        ></Form.Control>
       </Form.Group>
       <Form.Group>
-          <Form.Label>Date</Form.Label>
-          <Form.Control type="date"></Form.Control>
+        <Form.Label>Date</Form.Label>
+        <Form.Control onChange={handleChange} id="date" name="date" type="date"></Form.Control>
       </Form.Group>
-      <Button variant="primary" className="mt-2" size="md">Add Transaction</Button>
+      <Button variant="primary" type="submit" className="mt-2" size="md">
+        Add Transaction
+      </Button>
     </Form>
   );
 };
