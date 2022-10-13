8ff69fb7570eb5ec67cd0a9afd696cbb06505e44
lichaa824 <lisandrorubi@gmail.com>
2022-10-13 10:08:45 -0300

    LastTenTable replace with TransactionsTable

--- frontend/budget-app/src/components/Home/Home.js
+++ frontend/budget-app/src/components/Home/Home.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import transactionService from "../../services/transaction";
-import LastTenTable from "./LastTenTable";
 import Balance from "./Balance";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./Home.styles.css";
+import TransactionsTable from "../TransactionsTable";
 
 const Home = () => {
   const [balance, setBalance] = useState(0);
@@ -30,7 +30,7 @@ const Home = () => {
       <Row className="pt-5">
         {" "}
         <h5>Your last ten transactions are: </h5>
-        <LastTenTable />
+        <TransactionsTable limit={10} />
       </Row>
       <Row className="pt-5">
         <Button href="/abm" variant="primary" size="lg">
