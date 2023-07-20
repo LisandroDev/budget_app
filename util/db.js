a756adbcc2cf4f6d1637b2bbda3c6542439769e0
lichaa824 <lisandrorubi@gmail.com>
2023-07-19 21:35:23 -0300

    add pg

--- util/db.js
+++ util/db.js
@@ -1,5 +1,6 @@
 const { DATABASE_URL } = require("./config");
 const Sequelize = require("sequelize");
+const pg = require('pg');
 const { Umzug, SequelizeStorage } = require("umzug");
 
 const sequelize = new Sequelize(DATABASE_URL, {
